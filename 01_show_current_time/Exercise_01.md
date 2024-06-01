0dd320c084e69475ac9316d8586d08ac00a6e2e7
bganguly <gangulybikramjit@hotmail.com>
2024-05-31 21:14:24 -0700

    updated exercise_01 by also showing time till update

--- 01_show_current_time/Exercise_01.md
+++ 01_show_current_time/Exercise_01.md
@@ -1,5 +1,5 @@
 
-Build a react app that shows the current date time (which changes every few second) centered in view port.  
+Build a react app that shows the current date time (which changes every 3 seconds) centered in view port.  
 date-time string format can be either ISO or any other format.  
 the date-time line should render with any fixed-width font, so that there are no momentary jarring etc.
-Mention some room for improvent.
+Mention some room for improvement (setInterval cleanup etc).
--- 01_show_current_time/src/components/CurrentTime.js
+++ 01_show_current_time/src/components/CurrentTime.js
@@ -2,16 +2,17 @@ import { useState } from 'react';
 
 const CurrentTime = () => {
   const [timeNow, setTimeNow] = useState(new Date().toString())
+  const updateIntervalInSeconds = 3;
 
   const timeNowUpdater = () => {
     setTimeNow(new Date().toString())
   }
 
-  setInterval(timeNowUpdater, 3000)
+  setInterval(timeNowUpdater, updateIntervalInSeconds * 1000)
 
   return (
     <>
-    <div>Date Time now</div>
+    <div>Date Time : changes every {updateIntervalInSeconds} seconds</div>
     <div class="date-time-string">{timeNow}</div>
     </>
   )
