aa6a555363177cb1b9aaada7b944830de76dc970
bganguly <gangulybikramjit@hotmail.com>
2024-06-03 21:45:17 -0700

    updated concepts -> array_properties to be more self documenting

--- language_exercises/js/concepts/array_properties.js
+++ language_exercises/js/concepts/array_properties.js
@@ -1,28 +1,42 @@
 /*
-array for each/map/filter/reducer
+array for each/map/filter/reduce/Min
 */
 
 const inputArray = [-3,-2,-1,0,1,2,3]
 
+// demo forEach
 inputArray.forEach( element => {
+  // trying to get square root. negative numbers in input will return NaN
   console.log('using for each :', element, element ** 0.5)
 });
+console.log()
 
+// demo Array.map
 const outputUsingMap = inputArray.map( element => {
   return element ** 0.5
 });
 console.log('using Map :',outputUsingMap)
+console.log()
 
+// demo Array.filter
 const outputUsingFilter = inputArray.filter( element => {
   return element > 0
 });
 console.log('using Filter :', outputUsingFilter)
+console.log()
 
+// demo Array.reduce
 const outputUsingReduce = inputArray.reduce( (a,b) => {
   return Math.abs(a) + Math.abs(b);
 })
 console.log('using Reduce :', outputUsingReduce)
+console.log()
 
+// demo calling Math.min on an array object 
+// (Math.min/max take an enumerated list or multiple args. passing an array ref will nt work)
 const outputArrayMin1 = Math.min.apply(null,inputArray)
-const outputArrayMin2 = Math.min(...inputArray)
-console.log('using Min :', outputArrayMin1, outputArrayMin2)
+const outputArrayMin2 = Math.min.call(null,...inputArray)
+const outputArrayMin3 = Math.min(...inputArray)
+// below will return NaN
+const outputArrayMin4 = Math.min(inputArray)
+console.log('using Min :', outputArrayMin1, outputArrayMin2, outputArrayMin2, outputArrayMin4)
